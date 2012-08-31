857b636c5f1ffdc557d559cdae810de9eefe9dc3
Dharmesh Kamdar <dkamdar@hawk.iit.edu>
2012-08-31 15:32:14 -0500

    Added solutions.rkt

--- hw/basic-recursion/solutions.rkt
+++ hw/basic-recursion/solutions.rkt
@@ -1,3 +1,17 @@
+#lang racket
 (define (power x y)
-  ; your code here!
-  )
+  (cond ((eq? y 1) x)
+        (else (* x (power x (- y 1))))))
+
+(define (range x)
+  (cond ((eq? x 0) '0)
+        (else (cons (range(- x 1)) (cons x null)))))
+
+(define (append xx yy)
+  (cond ((eq? xx null) null
+        (eq? yy null) null)
+        (else (cons (xx yy)))))
+
+(define (reverse xx)
+  (cond ((eq? (cdr xx) null) xx)
+        (else (cons (reverse(cdr xx)) (car xx)))))
