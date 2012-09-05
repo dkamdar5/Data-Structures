a2822950e526c0b2163c004bf277cdf6cd74899e
Dharmesh Kamdar <dkamdar@hawk.iit.edu>
2012-09-05 12:37:50 -0500

    Fixed homework file with working append, range and reverse.

--- hw/basic-recursion/solutions.rkt
+++ hw/basic-recursion/solutions.rkt
@@ -3,15 +3,23 @@
   (cond ((eq? y 1) x)
         (else (* x (power x (- y 1))))))
 
-(define (range x)
-  (cond ((eq? x 1) 0)
-        (else (cons (range(- x 1)) (- x 1)))))
+(define (range xy)
+  (define (aux x y)
+    (cond ((eq? x -1) y)
+          (else (aux (- x 1) (cons x y)))))
+  (aux (- xy 1) null))
 
 (define (append xx yy)
   (cond ((eq? xx null) yy)
         ((eq? yy null) xx)
         (else (cons (car xx) (append (cdr xx) yy)))))
 
-(define (reverse xx)
+(define (reverse-bad xx)
   (cond ((eq? (cdr xx) null) xx)
-        (else (cons (reverse(cdr xx)) (car xx)))))
+        (else (append (reverse (cdr xx)) (list (car xx))))))
+
+(define (reverse-good xx)
+  (define (aux x y)
+    (cond ((eq? x null) y)
+          (else (aux (cdr x) (cons (car x) y)))))
+  (aux xx null))
