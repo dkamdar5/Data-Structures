afc0495051b27975c908b4090849ae4bb7b0dbb8
Dharmesh Kamdar <dkamdar@hawk.iit.edu>
2012-09-07 15:34:26 -0500

    Fixed flatten and finished immutable lists activity

--- hw/immutable-lists/solutions.rkt
+++ hw/immutable-lists/solutions.rkt
@@ -1,1 +1,15 @@
+#lang racket
+(define (delete-all key xx)
+  (cond ((eq? xx null) null)
+        ((eq? key (car xx)) (delete-all key (cdr xx)))
+        (else (cons (car xx) (delete-all key (cdr xx))))))
 
+(define (flatten xx)
+  (cond ((eq? xx null) null)
+        ((list? (car xx)) (append (flatten (car xx)) (flatten (cdr xx))))
+        (else (cons (car xx) (flatten (cdr xx))))))
+               
+(define (substitute xx from to)
+  (cond ((eq? xx null) null)
+        ((eq? from (car xx)) (substitute (cons to (cdr xx)) from to))
+        (else (cons (car xx) (substitute (cdr xx) from to)))))
