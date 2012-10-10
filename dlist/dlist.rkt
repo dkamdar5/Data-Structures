61efbfe82b2fc0067b4f52a53a4573d03d6d61ef
Dharmesh Kamdar <dkamdar@hawk.iit.edu>
2012-10-10 16:30:42 -0500

    More work

--- dlist/dlist.rkt
+++ dlist/dlist.rkt
@@ -11,27 +11,30 @@
       (begin
         (set-dnode-prev! sentinel sentinel)
         (set-dnode-next! sentinel sentinel)
-        (dlist sentinel sentinel 0))))
+        (dlist sentinel 0))))
 
   ; Here is a size function.  Just because I provide it doesn't mean it's complete!
   (define (size dl)
     (dlist-size dl))
+  
+  (define (inc-dlist-size! dl)
+    (set-dlist-size! dl (+ (dlist-size dl) 1)))
 
-  (define (insert-front xx elt)
-    (let ((node (dnode (dlist-sentinal xx) elt (dnode-next (dlist-sentinal xx)))))
+  (define (insert-front dl elt)
+    (let ((node (dnode (dlist-sentinel dl) elt (dnode-next (dlist-sentinel dl)))))
       ;node = new dnode. prev = sentinal. data = element. next = next dnode of sentinal.
       (begin
         (set-dnode-prev! (dnode-next node) node);set prev of the next node to itself
-        (set-dnode-next! (dlist-sentinel xx) node);set next of the sentinal to itself 
-        (inc-dlist-size! dlist))))
+        (set-dnode-next! (dlist-sentinel dl) node);set next of the sentinal to itself 
+        (inc-dlist-size! dl))))
   
   (define (insert-back xx elt)
-    (let ((node (dnode (dnode-prev (dlist-sentinal xx)) elt (dlist-sentinal xx))))
+    (let ((node (dnode (dnode-prev (dlist-sentinel xx)) elt (dlist-sentinel xx))))
       ;node = new dnode. prev = prev dnode of sentinal. data = element. next = sentinal.
       (begin
         (set-dnode-prev! (dlist-sentinel xx) node);set prev of sentinal to itselft
         (set-dnode-next! (dnode-prev node) node);set next of the prev node to itself
-        (inc-dlist-size! dlist))))
+        (inc-dlist-size! xx))))
   
   (define insert-ordered null)
   
