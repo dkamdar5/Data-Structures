18e96346f268c88f1e33deeef596a53aebcc17ef
Dharmesh Kamdar <dkamdar@hawk.iit.edu>
2012-11-16 16:29:22 -0600

    test cases try 1

--- bst/test-bst.rkt
+++ bst/test-bst.rkt
@@ -9,6 +9,10 @@
     (test-case "Initial dlist Properties"
     (let  ((bst (make-bst <)))
       (check-equal? (size bst) 0 "tree initially should have size zero.")
+      (add bst 1 'one)
+      (check-equal? (size bst) 1 "increment size")
+      (delete bst 1)
+      (check-equal? (size bst) 0 "decrement size")
       ))
 
 ))
