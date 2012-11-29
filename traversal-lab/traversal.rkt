3b0c8093d984ab607de00f4e002787f18a449e2f
Dharmesh Kamdar <dkamdar@hawk.iit.edu>
2012-11-29 16:20:24 -0600

    a

--- traversal-lab/traversal.rkt
+++ traversal-lab/traversal.rkt
@@ -11,14 +11,51 @@
 
   (struct tree (data left right) #:transparent)
 
-  (undefine make-tree)
-  (undefine add)
+  (define (make-tree)
+    (tree null '() '()))
+  
+  (define (add t elt)
+    (add-aux t elt))
+  (define (add-aux t elt)
+    (cond [(null? (tree-data t)) (tree elt null null)]
+          [(equal? elt (tree-data t)) (tree (tree-data t) (tree-left t) (tree-right t))]
+          [(< elt (tree-data t))
+           (if (null? (tree-left t)) (tree (tree-data t) (tree elt '() '()) (tree-right t))
+               (tree (tree-data t) (add-aux (tree-left t) elt) (tree-right t)))]
+          [(> elt (tree-data t))
+           (if (null? (tree-right t)) (tree (tree-data t) (tree-left t) (tree elt '() '()))
+               (tree (tree-data t) (tree-left t) (add-aux (tree-right t) elt)))]
+          (else (tree elt null null))))
+  
   (undefine traverse-dfs)
   (undefine traverse-bfs)
-  (undefine traverse-preorder)
+  (define (traverse-preorder t)
+    (let ([empty-stream (stream)])
+      (begin
+        (preorder-aux t (empty-stream)))))
+  (define (preorder-aux t next)
+    (if (null? (tree-data t)) next
+        (stream-append (null) null)
+        ))
+  
   (undefine traverse-inorder)
-  (undefine traverse-postorder)
+  
+  (define (traverse-postorder t)
+    (let ([empty-stream (stream)])
+      (begin
+        (postorder-aux t empty-stream))))
+  (define (postorder-aux t next)
+    (if (null? t) next
+        (stream-append (postorder-aux (tree-left t)
+            (postorder-aux (tree-right t) next)) (stream-cons (tree-data t) next))))
+  
   (undefine traverse-frontier)
+  
+  (define (stream-take st num)
+    (cond [(stream-empty? st) '()]
+          [(equal? num 0) '()]
+          (else (cons (stream-first st)
+                       (stream-take (stream-rest st) (- num 1))))))
 
   ; Test cases here
   (include "test-traversal.rkt")
